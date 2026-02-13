b8d2792d5c437a2a6f09bc5b3fb8cc5009663a9f
Brad Jones <brad@bjc.id.au>
2026-02-13 18:13:38 +1100

    feat(lib): inject DENOBRIDGE_VERSION constant for use in cdkts

--- lib/mod.ts
+++ lib/mod.ts
@@ -2,3 +2,5 @@ export * from "./providers/action.ts";
 export * from "./providers/datasource.ts";
 export * from "./providers/ephemeral_resource.ts";
 export * from "./providers/resource.ts";
+
+export const DENOBRIDGE_VERSION = "0.0.0";
